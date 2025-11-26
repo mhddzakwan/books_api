650c2ff06f1d9d0462bded34ee0a023212772732
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2025-11-27 00:54:51 +0700

    Refactor book API with improved comments and structure

--- app.js
+++ app.js
@@ -12,14 +12,24 @@ app.use(cors());
 // ? Pencarian Buku
 app.get("/books", async (req, res) => {
   const { title, author, publisher } = req.query;
+  // ? www.lab1.com/books?title=Kenangan&publisher=astonishing
 
   let query = "SELECT * FROM books";
+  // ? potongan kondisi SQL yang nantinya digabungkan menjadi bagian WHERE
   let conditions = [];
+
+  // ? nilai yang akan dimasukkan ke query placeholder $1, $2, dst.
+  // ! Misalnya "SELECT * FROM books WHERE title = 'kenangan' AND publisher = 'astonishing'"
+  // ! Misalnya "SELECT * FROM books WHERE title = $1 AND publisher = $2"
+  // ! Lebih aman, /books?title=a'; DROP TABLE books; --
   let values = [];
 
   if (title) {
     conditions.push(`title ILIKE $${values.length + 1}`);
     values.push(`%${title}%`);
+    // ? % = wildcard, teks yang mengandung value tsb
+    // ? condition = [title ILIKE $1]
+    // ? values = ['%kenangan%']
   }
   if (author) {
     conditions.push(`author ILIKE $${values.length + 1}`);
@@ -28,16 +38,24 @@ app.get("/books", async (req, res) => {
   if (publisher) {
     conditions.push(`publisher ILIKE $${values.length + 1}`);
     values.push(`%${publisher}%`);
+    // ? condition = [title ILIKE $1, publisher ILIKE $2]
+    // ? values = ['%kenangan%', '%astonishing%']
   }
 
   if (conditions.length > 0) {
     query += " WHERE " + conditions.join(" AND ");
   }
+  // ? query = SELECT * FROM books WHERE title ILIKE $1 AND publisher ILIKE $2
 
+  // ! try-catch = Error handling untuk mencegah program berhenti ketika terjadi error.
   try {
+    // ? berisi kode yang mungkin menimbulkan error ketika dijalankan.
+    // ? menjalankan query ke database
     const result = await pool.query(query, values);
+    // ! await = Menunggu hasil dari Postgre
 
     if (result.rows.length === 0) {
+
       return res.status(200).json({
         error: false,
         message: "Tidak ada hasil yang ditemukan",
@@ -51,6 +69,8 @@ app.get("/books", async (req, res) => {
       data: result.rows,
     });
   } catch (err) {
+    // ? berisi kode yang akan dijalankan jika terjadi error di dalam blok try
+    // ! Internal Server Error
     console.error(err.message);
     res.status(500).json({ error: true, message: "Server Error" });
   }
@@ -143,9 +163,14 @@ app.delete("/books/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const result = await pool.query("DELETE FROM books WHERE id=$1 RETURNING *", [id]);
+    const result = await pool.query(
+      "DELETE FROM books WHERE id=$1 RETURNING *",
+      [id]
+    );
     if (result.rows.length === 0) {
-      return res.status(404).json({ error: true, message: "Buku tidak ditemukan" });
+      return res
+        .status(404)
+        .json({ error: true, message: "Buku tidak ditemukan" });
     }
     res.status(200).json({
       error: false,
