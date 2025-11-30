1f9fc236d069a5f362e96e0afa67cdc03e60637a
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2025-11-30 21:43:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,185 @@
-const { Pool } = require("pg");
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
 
-// ? .env = menyimpan data konfigurasi agar tidak langsung di tuliskan dalam kode
-// ? Dapat disebut juga variabel "global"
-const dotenv = require("dotenv");
+const app = express();
+// ? Middleware
+app.use(express.json());
+app.use(cors());
 
-// ? Aktifkan dotenv
-dotenv.config();
+// ! API
 
-// ? Koneksi ke postgreSQL
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  // port: process.env.PGPORT,
+// ? Pencarian Buku
+app.get("/books", async (req, res) => {
+  const { title, author, publisher } = req.query;
+  // ? www.lab1.com/books?title=Kenangan&publisher=astonishing
+
+  let query = "SELECT * FROM books ";
+  // ? potongan kondisi SQL yang nantinya digabungkan menjadi bagian WHERE
+  let conditions = [];
+
+  // ? nilai yang akan dimasukkan ke query placeholder $1, $2, dst.
+  // ! Misalnya "SELECT * FROM books WHERE title = 'kenangan' AND publisher = 'astonishing'"
+  // ! Misalnya "SELECT * FROM books WHERE title = $1 AND publisher = $2"
+  // ! Lebih aman, /books?title=a'; DROP TABLE books; --
+  let values = [];
+
+
+  if (title) {
+    conditions.push(`title ILIKE $${values.length + 1}`);
+    values.push(`%${title}%`);
+    // ? % = wildcard, teks yang mengandung value tsb
+    // ? condition = [title ILIKE $1]
+    // ? values = ['%kenangan%']
+  }
+  if (author) {
+    conditions.push(`author ILIKE $${values.length + 1}`);
+    values.push(`%${author}%`);
+  }
+  if (publisher) {
+    conditions.push(`publisher ILIKE $${values.length + 1}`);
+    values.push(`%${publisher}%`);
+    // ? condition = [title ILIKE $1, publisher ILIKE $2]
+    // ? values = ['%kenangan%', '%astonishing%']
+  }
+
+  if (conditions.length > 0) {
+    query += " WHERE " + conditions.join(" AND ");
+  }
+  // ? query = SELECT * FROM books WHERE title ILIKE $1 AND publisher ILIKE $2
+
+  // ! try-catch = Error handling untuk mencegah program berhenti ketika terjadi error.
+  try {
+    // ? berisi kode yang mungkin menimbulkan error ketika dijalankan.
+    // ? menjalankan query ke database
+    const result = await pool.query(query, values);
+    // ! await = Menunggu hasil dari Postgre
+    
+    if (result.rows.length === 0) {
+      return res.status(200).json({
+        error: false,
+        message: "Tidak ada hasil yang ditemukan",
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: "Data berhasil ditemukan",
+      data: result.rows,
+    });
+  } catch (err) {
+    // ? berisi kode yang akan dijalankan jika terjadi error di dalam blok try
+    // ! Internal Server Error
+    console.error(err.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
 });
 
-module.exports = pool;
+// ? Menampilkan buku berdasarkan id
+app.get("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("SELECT * FROM books WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Buku tidak ditemukan" });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: "Data berhasil ditemukan",
+      data: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
+});
+
+// ? Menambahkan buku
+app.post("/books", async (req, res) => {
+  const { title, author, publish_date, publisher } = req.body;
+
+  if (!title || !author || !publish_date || !publisher) {
+    return res.status(400).json({
+      error: true,
+      message: "Semua field wajib diisi",
+    });
+  }
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO books (title, author, publish_date, publisher) 
+       VALUES ($1, $2, $3, $4) RETURNING *`,
+      [title, author, publish_date, publisher]
+    );
+
+    res.status(201).json({
+      error: false,
+      message: "Buku berhasil ditambahkan",
+      data: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
+});
+
+// ? Mengedit buku berdasarkan id
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, publish_date, publisher } = req.body;
+
+    const result = await pool.query(
+      `UPDATE books SET title = $1, author = $2, publish_date = $3, publisher = $4 
+       WHERE id = $5 RETURNING *`,
+      [title, author, publish_date, publisher, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Buku tidak ditemukan" });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: "Buku berhasil diperbarui",
+      data: result.rows[0],
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
+});
+
+// ? Menghapus buku
+app.delete("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM books WHERE id=$1 RETURNING *",
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: true, message: "Buku tidak ditemukan" });
+    }
+    res.status(200).json({
+      error: false,
+      message: "Buku berhasil dihapus",
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
+});
+
+module.exports = app;
