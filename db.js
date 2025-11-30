77f3bffe748a96a8d4a5f1aef71ebb8529814e69
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2025-11-30 21:43:13 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
 const { Pool } = require("pg");
+
+// ? .env = menyimpan data konfigurasi agar tidak langsung di tuliskan dalam kode
+// ? Dapat disebut juga variabel "global"
 const dotenv = require("dotenv");
 
+// ? Aktifkan dotenv
 dotenv.config();
 
+// ? Koneksi ke postgreSQL
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
+  
   ssl: {
     rejectUnauthorized: false,
   },
